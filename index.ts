474d69931dc6dda2be04b064c087b1b3773920bb
Leaf48 <58620209+Leaf48@users.noreply.github.com>
2022-09-03 20:29:12 +0900

    a

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express from "express"
+import path from "path"
+import bodyParser from "body-parser"
 
 
 const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.set("views", "./views")
+app.set("view engine", "pug")
+
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    // res.send("hello world")
+    // res.sendFile(path.join("index"))
+    res.render("index")
+})
+app.post("/", (req, res) => {
+    // const word = req.body.word;
+    console.log(req.body)
+    res.render("index")
 })
 
 app.listen(3000, () => {
